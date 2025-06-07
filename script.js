d987c841abd98b244e3569789a7919d09393e796
ron <lucifron28@gmail.com>
2025-06-07 12:23:41 +0800

    add button array

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  
+   const calculatorButtons = [
+      ['C', '', '', '/'],
+      ['7', '8', '9', '*'],
+      ['4', '5', '6', '-'],
+      ['1', '2', '3', '+'],
+      ['0', '.', '=', '']
+   ];
+
    const buttonGrid = document.getElementById("button-grid");
    
 });
