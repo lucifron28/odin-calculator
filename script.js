e3c51ce3ce599b957c633b1e34089dbb94e05e38
ron <lucifron28@gmail.com>
2025-06-07 20:28:21 +0800

    fix the button layout

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
      const buttonLayout = [
-      ['C', '±', '%', '÷'],
-      ['7', '8', '9', '×'],
-      ['4', '5', '6', '−'],
-      ['1', '2', '3', '+'],
-      ['0', '.', '=']
+      'C', '±', '%', '÷',
+      '7', '8', '9', '×',
+      '4', '5', '6', '−',
+      '1', '2', '3', '+',
+      '0', '.', '='
    ];
 
 
    const root = document.getElementById("root");
-   const buttonGrid = document.getElementById("button-grid");
-   
+   function createCalculatorContainer() {
+      const calculatorDiv = document.createElement("div");
+      calculatorDiv.classList.add("calculator-container");
+      return calculatorDiv;
+   }
+
    function createButton(buttonText) {
       const newButton = document.createElement("button");
       newButton.textContent = buttonText;
@@ -20,13 +24,16 @@ document.addEventListener("DOMContentLoaded", () => {
    function createButtonGrid(buttons) {
       const buttonGrid = document.createElement("div");
       buttonGrid.classList = "button-grid";
-      root.appendChild(buttonGrid);
 
       buttons.forEach((btn) => {
-         const newBtn = document.createElement("button");
-         newBtn.classList.add("calculator-button");
-         newBtn.textContent = btn;
+         const newBtn = createButton(btn);
          buttonGrid.appendChild(newBtn);
-      })
+      });
+      return buttonGrid;
    }
+
+   const calculator = createCalculatorContainer();
+   const buttonGrid = createButtonGrid(buttonLayout);
+   calculator.appendChild(buttonGrid);
+   root.appendChild(calculator);
 });
