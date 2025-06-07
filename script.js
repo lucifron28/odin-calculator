67966dfad015a28d01fa4012c182f00ea92ecea2
ron <lucifron28@gmail.com>
2025-06-07 12:37:29 +0800

    change the button layout

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-   const calculatorButtons = [
-      ['C', '', '', '/'],
-      ['7', '8', '9', '*'],
-      ['4', '5', '6', '-'],
+     const buttonLayout = [
+      ['C', '±', '%', '÷'],
+      ['7', '8', '9', '×'],
+      ['4', '5', '6', '−'],
       ['1', '2', '3', '+'],
-      ['0', '.', '=', '']
+      ['0', '.', '=']
    ];
 
+
+   const root = document.getElementById("root");
    const buttonGrid = document.getElementById("button-grid");
    
+   function createButton(buttonText) {
+      const newButton = document.createElement("button");
+      newButton.textContent = buttonText;
+      return newButton;
+   }
 });
